7fab4e36480161d1a36c39fa2575dacbc857dff7
Sajson <amokrzycki96@gmail.com>
2022-10-13 11:01:26 +0200

    Added revenue section, stacked and sparkline charts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,3 +12,4 @@ export { default as ColorMapping } from "./Charts/ColorMapping";
 export { default as Financial } from "./Charts/Financial";
 export { default as Line } from "./Charts/Line";
 export { default as Pie } from "./Charts/Pie";
+export { default as Stacked } from "./Charts/Stacked";
